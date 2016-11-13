285fae6c552b9d5e006312af1f13e63d83ba3cfc
David Lin <thunderbirdavid@gmail.com>
2016-11-13 00:51:59 -0800

    update

---
+++ static/script.js
@@ -0,0 +1,20 @@
+function search(ele) {
+    if(event.keyCode == 13) {
+        //alert(ele.value);
+        document.getElementById("myform").submit();
+    }
+}
+
+function datestart(ele) {
+    if(event.keyCode == 13) {
+        //alert(ele.value);
+        document.getElementById("datestartform").submit();
+    }
+}
+
+function dateend(ele) {
+    if(event.keyCode == 13) {
+        //alert(ele.value);
+        document.getElementById("dateendform").submit();
+    }
+}
